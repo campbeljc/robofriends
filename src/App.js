129bddfde292a9e66c5c95dfe71f0f4b455e4f6d
campbeljc <40182384+campbeljc@users.noreply.github.com>
2020-11-04 15:55:14 -0800

    adding initial files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import CardList from "./CardList.js";
+import {robots} from "./robots.js";
+import SearchBox from "./SearchBox.js";
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: " "
+    }
+  }
+
+onSearchChange = (event) => {
+  this.setState({ searchfield: event.target.value})
+}
+
+render() {
+  const filteredRobots = this.state.robots.filter(robots =>{
+    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+  })
+  return (
+    <div className = 'tc'>
+      <h1 className = 'f2'>RoboFriends</h1>
+      <SearchBox searchChange={this.onSearchChange}/>
+      <CardList robots={filteredRobots}/>
+    </div>
+  );
+}
+}
+
+export default App;
