4eed0b50e57fd01677df722c37fa1f9e4376a6cf
campbeljc <40182384+campbeljc@users.noreply.github.com>
2020-11-13 15:38:59 -0800

    adding work from Video: React App 4

--- src/App.js
+++ src/App.js
@@ -8,27 +8,33 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: " "
     }
+  } 
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+    .then(users => this.setState({ robots: users}));
   }
 
-onSearchChange = (event) => {
-  this.setState({ searchfield: event.target.value})
-}
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value})
+  }
 
-render() {
-  const filteredRobots = this.state.robots.filter(robots =>{
-    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-  })
-  return (
-    <div className = 'tc'>
-      <h1 className = 'f2'>RoboFriends</h1>
-      <SearchBox searchChange={this.onSearchChange}/>
-      <CardList robots={filteredRobots}/>
-    </div>
-  );
-}
+  render() {
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className = 'tc'>
+        <h1 className = 'f2'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
 }
 
 export default App;
